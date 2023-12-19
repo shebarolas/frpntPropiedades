d544e2b4bc4b940686e85394563ad7ade8623683
shebarolas <30813367+shebarolas@users.noreply.github.com>
2023-12-18 22:14:47 -0300

    Mejora pagina admin

--- src/components/ComponentsAdmin/displayAdmin/DisplayAdmin.jsx
+++ src/components/ComponentsAdmin/displayAdmin/DisplayAdmin.jsx
@@ -2,31 +2,47 @@ import React from 'react'
 import "./displayAdmin.css";
 import { EditProp } from '../editProp/EditProp';
 import { DeleteApartment } from '../deleteAparment/DeleteApartment';
-
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlinePriceChange } from "react-icons/md";
+import { Image } from 'antd';
+import { Carousel } from 'antd';
 
 export const DisplayAdmin = ({ data, setLoad }) => {
   const { photos } = data;
   console.log(data);
-  console.log(photos);
   return (
     <div className='displayAdmin'>
       <div className="displayWrap">
         <div className="dWrapTitle">
+          <div className="dImages">
+            <Carousel autoplay className='borderr'>
+             {
+                photos.map((photo, i) => (
+                  <Image
+                    height={320}
+                    width={400}
+                    style={{ borderRadius: '8px' }}
+                    src={photo} key={i} className='imgs'
+                  />
+                ))
+              } 
+            </Carousel>
+          </div>
           <div className="dTitles">
             <h1 className='dTitle'>{data.nombre}</h1>
-            <span>{data.direccion}, {data.ciudad}</span>
-            <span>{data.desc}</span>
-          </div>
-          <div className="dImages">
-            {
-              photos.map((photo, i) => (
-                <img className='imgs' src={photo} key={i} alt="" />
-              ))
-            }
+            <div className="disLocation">
+              <IoLocationOutline />
+              <span>{data.direccion}, {data.ciudad}</span>
+            </div>
+            <div className="disDesc">
+              <MdOutlinePriceChange />
+              <span>{data.valor}</span>
+            </div>
+
           </div>
         </div>
         <div className="dBtns">
-          <EditProp setLoad={setLoad} data={data}/>
+          <EditProp setLoad={setLoad} data={data} />
           <DeleteApartment setLoad={setLoad} data={data} />
         </div>
       </div>
--- src/components/ComponentsAdmin/verAparment/VerAparment.jsx
+++ src/components/ComponentsAdmin/verAparment/VerAparment.jsx
@@ -37,18 +37,20 @@ export const VerAparment = () => {
 
   return (
     <div className="ver">
-      <CreateAp setLoad={setLoad}/>
+      <CreateAp setLoad={setLoad} />
       <div className='verPropiedades'>
-        <h1>Mis Propiedades</h1>
-        {
-          loading ? <Skeleton /> : <>
-            {
-              data.map(data => (
-                <DisplayAdmin setLoad={setLoad} data={data} key={data._id} />
-              ))
-            }
-          </>
-        }
+        <h1 className='verText'>Mis Propiedades</h1>
+        <div className="verProps">
+          {
+            loading ? <Skeleton /> : <>
+              {
+                data.map(data => (
+                  <DisplayAdmin setLoad={setLoad} data={data} key={data._id} />
+                ))
+              }
+            </>
+          }
+        </div>
       </div>
     </div>
   )
